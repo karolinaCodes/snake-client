6d09ac2d32d19072b506f5e9948ea96fe9231e17
karolinaCodes <karolinaswislocki@hotmail.com>
2021-11-02 18:22:33 +0000

    refactor play.js to client.js where we moved the game connecting functionality

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-const net = require("net");
+// const net = require("net");
+const connect = require("./client");
+// const {setupInput} = require("./input");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: "50541",
-  });
+// console.log("Connecting ...");
 
-  conn.on("connect", () => {
-    console.log("Connected!");
-  });
-
-  conn.on("data", data => {
-    console.log(data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+// setupInput(connect());
